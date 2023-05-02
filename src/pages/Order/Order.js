d29e0c69ff16afb4347243c3f277823d5cbb0fe4
yoohmoon <ansekfzhd5455@naver.com>
2023-05-02 23:18:02 +0900

    Modify : order.js 레이아웃 주문자 정보까지 중간 푸시

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -2,7 +2,50 @@ import React from "react";
 import "./Order.scss";
 
 function Order() {
-  return <div className="order">Order Page</div>;
+  return (
+    <div className="order">
+      <header>
+        <ul className="orderNav">
+          <li className="orderNavActive">
+            <a>배송 정보</a>
+          </li>
+          <img
+            className="orderNavArrow"
+            src="/images/order/arrow-small.png"
+            alt="arrow"
+          />
+          <span />
+          <li className="orderNavInactive">
+            <a>결제 방법</a>
+          </li>
+          <img
+            className="orderNavArrow"
+            src="/images/order/arrow-small.png"
+            alt="arrow"
+          />
+          <li className="orderNavInactive">
+            <a>주문 확인</a>
+          </li>
+        </ul>
+      </header>
+      <div className="orderBox">
+        <div className="orderBoxLeft">
+          <h2 className="orderBoxLeftTitle">주문을 어디로 보내시겠습니까?</h2>
+          <form className="orderBoxLeftForm">
+            <h3>주문자 정보</h3>
+            <div className="inputNameWrap">
+              <input placeholder="성" />
+              <input placeholder="이름" />
+            </div>
+            <h3>배송지 정보</h3>
+          </form>
+        </div>
+        <div className="loginBoxRight">
+          <div>주문내역 박스(예정)</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Order;
