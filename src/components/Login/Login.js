1c7b2223c48dc18fbe176aaceb3253829a23fc4c
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-05-02 21:34:44 +0900

    ADD : 로그인 레이아웃

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import './Login.scss';
+import React from "react";
+import "./Login.scss";
 
 function Login() {
-  return <div className="login">Login Page</div>;
+  return (
+    <>
+      <div className="login">
+        <div className="loginContent">
+          <h2 className="loginTitle">로그인</h2>
+          <form className="loginForm">
+            <input
+              type="text"
+              name="email"
+              className="loginEmail"
+              placeholder=" 이메일 주소"
+            ></input>
+            <input
+              type="password"
+              name="userPassword"
+              className="loginPw"
+              placeholder=" 비밀번호"
+            ></input>
+          </form>
+          <BigButton name={"로그인"} buttonColor={"dark"} />
+          <BigButton name={"가입하기"} buttonColor={"bright"} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
