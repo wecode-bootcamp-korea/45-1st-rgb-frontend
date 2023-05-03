655c71a71b8503af81088ff1610072b6463a1e61
yoohmoon <ansekfzhd5455@naver.com>
2023-05-03 23:13:10 +0900

    Add : Order Delivery 배송지 입력 컴포넌튼 분리 및 레이아웃 완성

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -16,46 +16,7 @@ function Order() {
           src="/images/Order/arrow2.png"
         />
         <Delivery />
-        <Payment />
-        {/* left section 컴포넌트 분리하기 */}
-        {/*  <div className="orderBoxLeft">
-          <h2 className="orderBoxLeftTitle">주문을 어디로 보내시겠습니까?</h2>
-          <form className="orderInfoForm">
-            <h3>주문자 정보</h3>
-            <div className="inputNameWrap">
-              <input type="text" placeholder="성" />
-              <input type="text" placeholder="이름" />
-            </div>
-            <input type="text" placeholder="전화번호" />
-          </form>
-
-          <form className="addressForm">
-            <h3>배송지 정보</h3>
-            <div className="addressInput">
-              <input type="text" placeholder="주소" />
-            </div>
-            <div className="addressDetailInput">
-              <input type="text" placeholder="상세주소" />
-            </div>
-            <label>
-              <input type="checkbox" size="10" maxLength="10" />이 주소를 기본
-              배송지로 저장합니다
-            </label>
-            <label>
-              <input type="checkbox" />
-              주문자 정보와 배송지 정보가 동일합니다
-            </label>
-          </form>
-
-          <form>
-            <label>
-              <input type="checkbox" />
-              배송 기사님께 요청 사항이 있습니다
-              <textarea />
-            </label>
-          </form>
-        </div> */}
-
+        {/* <Payment /> */}
         <OrderList />
       </div>
     </div>
--- src/pages/Order/components/Delivery/Delivery.js
+++ src/pages/Order/components/Delivery/Delivery.js
@@ -2,7 +2,58 @@ import React from "react";
 import "./Delivery.scss";
 
 function Delivery() {
-  return <div className="delivery">Delivery</div>;
+  return (
+    <div className="delivery">
+      <h2 className="deliveryTitle">주문을 어디로 보내시겠습니까?</h2>
+      <form className="deliveryForm">
+        <div className="userInfoForm">
+          <h3>주문자 정보</h3>
+          <div className="inputNameWrap">
+            <input className="familyName" type="text" placeholder="성" />
+            <input className="firstName" type="text" placeholder="이름" />
+          </div>
+          <input className="phoneNumber" type="text" placeholder="전화번호" />
+        </div>
+
+        <div className="deliveryInfoForm">
+          <h3>배송지 정보</h3>
+          <div className="addressInput">
+            <input type="text" placeholder="주소" />
+          </div>
+          <div className="addressDetailInput">
+            <input
+              type="text"
+              placeholder="상세주소(아파트 동, 호수, 일반 주택 등)"
+            />
+          </div>
+        </div>
+
+        <label>
+          <input type="checkbox" size="10" maxLength="10" />이 주소를 기본
+          배송지로 저장합니다
+        </label>
+        <div>
+          <label>
+            <input type="checkbox" />
+            주문자 정보와 배송지 정보가 동일합니다
+          </label>
+        </div>
+        <br />
+
+        <div className="requestInputWrapper">
+          <label>
+            <input type="checkbox" />
+            배송 기사님께 요청 사항이 있습니다
+          </label>
+          <input type="text" className="requestInput" />
+        </div>
+      </form>
+
+      <form className="addressForm" />
+
+      <form />
+    </div>
+  );
 }
 
 export default Delivery;
