413f9234125f864a952f72d10c844194f5b6d607
Jinny-Jin <katie9796@naver.com>
2023-05-03 23:10:37 +0900

    ADD : Product List 페이지 레이아웃 완성

--- src/pages/ProductList/Component/LeftFilter/LeftFilter.js
+++ src/pages/ProductList/Component/LeftFilter/LeftFilter.js
@@ -10,22 +10,75 @@ function LeftFilter({ Show }) {
       </div>
       <div className="filters">
         <div className="filterContent">
-          <span>Price</span>
-          <button>arrow</button>
-          <p>0원 - 2만원</p>
-          <p>2만원 - 5만원</p>
+          <div className="priceAndButton">
+            <span>Price</span>
+            <button>
+              <img alt="button" src="/images/productList/Expand Arrow.png" />
+            </button>
+          </div>
+          <div className="contentButton">
+            <label for="price1">0만원 - 2만원</label>
+            <input
+              type="radio"
+              id="price1"
+              name="priceCheck"
+              value="price1"
+              checked
+            />
+          </div>
+          <div className="contentButton">
+            <label for="price2">2만원 - 5만원</label>
+            <input type="radio" id="price2" name="priceCheck" value="price2" />
+          </div>
         </div>
         <div className="filterContent">
-          <span>Size</span>
-          <button>arrow</button>
-          <p>0원 - 2만원</p>
-          <p>2만원 - 5만원</p>
+          <div className="priceAndButton">
+            <span>Size</span>
+            <button>
+              <img alt="button" src="/images/productList/Expand Arrow.png" />
+            </button>
+          </div>
+          <div className="contentButton">
+            <label for="size1">0만원 - 2만원</label>
+            <input
+              type="radio"
+              id="size1"
+              name="sizeCheck"
+              value="size1"
+              checked
+            />
+          </div>
+          <div className="contentButton">
+            <label for="size2">2만원 - 5만원</label>
+            <input type="radio" id="size2" name="sizeCheck" value="size2" />
+          </div>
         </div>
         <div className="filterContent">
-          <span>Material</span>
-          <button>arrow</button>
-          <p>0원 - 2만원</p>
-          <p>2만원 - 5만원</p>
+          <div className="priceAndButton">
+            <span>Material</span>
+            <button>
+              <img alt="button" src="/images/productList/Expand Arrow.png" />
+            </button>
+          </div>
+          <div className="contentButton">
+            <label for="material1">0만원 - 2만원</label>
+            <input
+              type="radio"
+              id="material1"
+              name="materialCheck"
+              value="material1"
+              checked
+            />
+          </div>
+          <div className="contentButton">
+            <label for="material2">2만원 - 5만원</label>
+            <input
+              type="radio"
+              id="material2"
+              name="materialCheck"
+              value="material2"
+            />
+          </div>
         </div>
       </div>
     </div>
