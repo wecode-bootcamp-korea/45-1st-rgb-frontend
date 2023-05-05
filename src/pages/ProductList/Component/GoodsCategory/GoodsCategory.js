5c5d429f6b56caee543fcacb262aa5688626e445
Jinny-Jin <katie9796@naver.com>
2023-05-05 21:31:22 +0900

    ADD : Goods 카테고리에 있을 때 하위 카테고리가 나타나는 레이아웃 추가

---
+++ src/pages/ProductList/Component/GoodsCategory/GoodsCategory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function GoodsCategory() {
+  return (
+    <div className="goodsCategory">
+      <button>Living Goods</button>
+      <button>Phone Case</button>
+      <button>Poster</button>
+    </div>
+  );
+}
+
+export default GoodsCategory;
--- src/pages/ProductList/Component/ListBottom/ListBottom.js
+++ src/pages/ProductList/Component/ListBottom/ListBottom.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
-import "./ListBottom.scss";
 import ArtWorks from "../ArtWorks/ArtWorks";
 import LeftFilter from "../LeftFilter/LeftFilter";
+import GoodsCategory from "../GoodsCategory/GoodsCategory";
+import "./ListBottom.scss";
 
 function ListBottom() {
+  // Art Works 와 Goods 카테고리에 따른 컴포넌트 변화를 위한 useState
   const [category, setCategory] = useState("Artlist");
   const [artContent, setArtContent] = useState([]);
   const [goodsContent, setGoodsContent] = useState([]);
   const [onOff, setOnOff] = useState(artContent);
+  // Goods 카테고리의 하위 카테고리 보이고 안보이기 위한 useState
+  const [show, setShow] = useState(false);
 
   const artlist = () => {
     setCategory("Artlist");
@@ -39,16 +43,21 @@ function ListBottom() {
   useEffect(() => {
     if (category === "Artlist") {
       setOnOff(artContent);
+      setShow(false);
     } else if (category === "Goodslist") {
       setOnOff(goodsContent);
+      setShow(true);
     }
   }, [artContent, goodsContent, category]);
 
   return (
     <div className="listBottom">
       <div className="bottomTop">
-        <span onClick={artlist}>Art Works</span>
-        <span onClick={goodslist}>Goods</span>
+        <div className="category">
+          <span onClick={artlist}>Art Works</span>
+          <span onClick={goodslist}>Goods</span>
+        </div>
+        {show && <GoodsCategory />}
       </div>
       <div className="bottomBottom">
         <div className="bottomLeft">
