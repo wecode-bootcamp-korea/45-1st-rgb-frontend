cb7799c2a705ab11ef7841031590b2ffbf3bdd80
mac <mac@macui-MacBookAir.local>
2023-05-05 16:10:21 +0900

    Add: Cart 기능 구현

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -1,41 +1,91 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import CartSum from "./CartSum";
 import "./Cart.scss";
 
 export default function CartList() {
-  const [number, setNumber] = useState(1);
-  const [totalPrice, setTotalPrice] = useState(790000);
+  const [items, setItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  const decrement = () => {
-    if (number > 1) {
-      setNumber(number - 1);
-      setTotalPrice((number - 1) * 8000000);
+  useEffect(() => {
+    fetch("./data/mockData.json", {
+      method: "GET"
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   fetch("http://10.58.52.141:3000/ping", {
+  //     method: "GET"
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => localStorage.setItem("TOKEN", data.accessToken));
+  // }, []);
+
+  useEffect(() => {
+    let total = 0;
+    items.forEach((item) => {
+      total += item.price * item.count;
+    });
+    setTotalPrice(total);
+  }, [items]);
+
+  const decrement = (index) => {
+    const newItems = [...items];
+    if (newItems[index].count > 1) {
+      newItems[index].count--;
+      setItems(newItems);
+      setTotalPrice(cartTotalPrice(newItems));
     }
   };
-  const increment = () => {
-    setNumber(number + 1);
-    setTotalPrice((number + 1) * 8000000);
+
+  const increment = (index) => {
+    const newItems = [...items];
+    newItems[index].count++;
+    setItems(newItems);
+    setTotalPrice(cartTotalPrice(newItems));
+  };
+
+  const cartTotalPrice = (items) => {
+    let total = 0;
+    items.forEach((item) => {
+      total += item.price * item.count;
+    });
+    return total;
+  };
+
+  const deleteItem = (index) => {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+    setTotalPrice(cartTotalPrice(newItems));
   };
 
   return (
     <>
-      <div className="cartItem">
-        <div className="itemName">Product 1</div>
-        <div className="itemSize">220 x 480 / 아크릴</div>
-        <div className="itemQuantity">
-          <button className="minus" onClick={decrement}>
-            -
-          </button>
-          {number}
-          <button className="plus" onClick={increment}>
-            +
-          </button>
+      {items.map((item, id) => (
+        <div key={id} className="cartItem">
+          <div className="itemName">{item.title}</div>
+          <div className="itemSize">
+            {item.size}/{item.material}
+          </div>
+          <div className="itemQuantity">
+            <button className="minus" onClick={() => decrement(id)}>
+              -
+            </button>
+            {item.count}
+            <button className="plus" onClick={() => increment(id)}>
+              +
+            </button>
+          </div>
+          <div className="itemPrice">{item.price}원</div>
+          <div className="cartDelete">
+            <button className="deleteButton" onClick={() => deleteItem(id)} />
+          </div>
         </div>
-        <div className="itemPrice">{totalPrice}원</div>
-        <div className="cartDelete">
-          <button className="deleteButton" />
-        </div>
-      </div>
+      ))}
 
       <CartSum totalPrice={totalPrice} />
     </>
