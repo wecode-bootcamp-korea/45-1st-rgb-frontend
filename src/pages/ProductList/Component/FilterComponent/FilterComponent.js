e67ff33089db4804a9a7226a072f6dcd0a4e51b2
Jinny-Jin <katie9796@naver.com>
2023-05-05 20:55:54 +0900

    ADD : 좌측 필터 화살표 누르면 content 열렸다 닫히는 레이아웃 추가

--- src/pages/ProductList/Component/FilterComponent/FilterComponent.js
+++ src/pages/ProductList/Component/FilterComponent/FilterComponent.js
@@ -1,34 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 function FilterComponent({ content }) {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="filterContent">
       <div className="priceAndButton">
         <span>{content.title}</span>
-        <button>
-          <img alt="button" src="/images/productList/Expand Arrow.png" />
+        {/* 보이기 버튼 */}
+        <button
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          <img
+            alt="button"
+            src={`${
+              show
+                ? "/images/productList/Expand Arrow2.png"
+                : "/images/productList/Expand Arrow.png"
+            }`}
+          />
+
+          {/* <img alt="button" src="/images/productList/Expand Arrow.png" /> */}
         </button>
       </div>
-      <div className="contentButton">
-        <label for={`${content.content.first.id}`}>
-          {content.content.first.contents}
-        </label>
-        <input
-          type="radio"
-          id={`${content.content.first.id}`}
-          name={content.name}
-          checked
-        />
-      </div>
-      <div className="contentButton">
-        <label for={`${content.content.second.id}`}>
-          {content.content.second.contents}
-        </label>
-        <input
-          type="radio"
-          id={`${content.content.second.id}`}
-          name={content.name}
-        />
+      <div>
+        {show ? (
+          <>
+            <div className="contentButton">
+              <label for={`${content.content.first.id}`}>
+                {content.content.first.contents}
+              </label>
+              <input
+                type="radio"
+                id={`${content.content.first.id}`}
+                name={content.name}
+                checked
+              />
+            </div>
+            <div className="contentButton">
+              <label for={`${content.content.second.id}`}>
+                {content.content.second.contents}
+              </label>
+              <input
+                type="radio"
+                id={`${content.content.second.id}`}
+                name={content.name}
+              />
+            </div>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
