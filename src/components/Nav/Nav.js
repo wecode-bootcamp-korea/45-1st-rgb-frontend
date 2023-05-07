2176b9b9b1c3dfc2e780fe45aa7445b5c1ef506b
이원준 <wonjun@MacBook-Pro-2.local>
2023-05-07 13:37:27 +0900

    nav 수정중

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.scss";
 
-function Nav() {
-  return <div className="nav">Nav Component</div>;
-}
+const Nav = () => {
+  const [activeLink, setActiveLink] = useState("home");
+
+  const handleLinkClick = link => {
+    setActiveLink(link);
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="#main"
+            className={activeLink === "main" ? "active" : ""}
+            onClick={() => handleLinkClick("main")}
+          >
+            rgb.
+          </a>
+        </li>
+        <li>
+          <a
+            href="Artists의 파일주소"
+            className={activeLink === "Artists" ? "active" : ""}
+            onClick={() => handleLinkClick("Artists")}
+          >
+            Artists
+          </a>
+        </li>
+        <li>
+          <a
+            href="Shop의 파일주소"
+            className={activeLink === "Shop" ? "active" : ""}
+            onClick={() => handleLinkClick("Shop")}
+          >
+            Shop
+          </a>
+        </li>
+        <li>
+          <a
+            href="My Page의 파일주소"
+            className={activeLink === "My page" ? "active" : ""}
+            onClick={() => handleLinkClick("My page")}
+          >
+            My page
+          </a>
+        </li>
+        <li>
+          <a
+            href="Cart의 파일주소"
+            className={activeLink === "Cart" ? "active" : ""}
+            onClick={() => handleLinkClick("Cart")}
+          >
+            Cart
+          </a>
+        </li>
+        <li>
+          <a
+            href="Logout의 파일주소"
+            className={activeLink === "Logout" ? "active" : ""}
+            onClick={() => handleLinkClick("Logout")}
+          >
+            Logout
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 export default Nav;
