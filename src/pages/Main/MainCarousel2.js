c51798d7dd2adcfbe02b370c1e125ed37268694b
mac <mac@macui-MacBookAir.local>
2023-05-08 02:52:31 +0900

    Modify: Main 굿즈 캐러셀 구현

--- src/pages/Main/MainCarousel2.js
+++ src/pages/Main/MainCarousel2.js
@@ -1,18 +1,58 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./MainCarousel2.scss";
 import SecondCarousel from "./SecondCarousel";
 
-export default function MainCarasol2() {
-  const [data, setData] = useState([]);
+const TOTAL_SLIDES = 6;
+
+export default function MainCarousel2() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [carouselItem, setCarouselItem] = useState([]);
+  const slideRef = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("./data/productImage.json");
+      const json = await data.json();
+      setCarouselItem(json);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (slideRef.current) {
+      slideRef.current.style.transition = "transform 0.2s ease-in";
+      slideRef.current.style.transform = `translateX(-${currentSlide * 33.3}%)`;
+    }
+  }, [currentSlide]);
+
+  const nextSlide = () => {
+    if (currentSlide >= TOTAL_SLIDES) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide((prev) => prev + 1);
+    }
+  };
+
+  const prevSlide = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(TOTAL_SLIDES);
+    } else {
+      setCurrentSlide((prev) => prev - 1);
+    }
+  };
 
   return (
     <div className="goodsContainer">
-      <ul className="goodsBox">
-        <SecondCarousel />
-      </ul>
-      <div className="buttonContainer">
-        <button className="buttonPre2" />
-        <button className="buttonNext2" />
+      <div className="goodsBox">
+        <ul className="secondSlide" ref={slideRef}>
+          {carouselItem.map((item, id) => (
+            <SecondCarousel key={id} item={item} />
+          ))}
+        </ul>
+        <div className="buttonContainer">
+          <button className="buttonPre2" onClick={prevSlide} />
+          <button className="buttonNext2" onClick={nextSlide} />
+        </div>
       </div>
     </div>
   );
--- src/pages/Main/SecondCarousel.js
+++ src/pages/Main/SecondCarousel.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./SecondCarousel.scss";
 
-export default function SecondCarousel() {
+export default function SecondCarousel({ item }) {
+  const { img, size, title } = item;
+
   return (
     <li className="slideBox">
-      <img className="goodsImg" src="" alt="artist" />
+      <img className="goodsImg" src={img} alt="artist" />
       <div className="textBox">
         <div className="textBoxName">
-          title <div className="textBoxSize">size</div>
+          {title} <div className="textBoxSize">{size}</div>
         </div>
       </div>
     </li>
