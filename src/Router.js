3c1122095f800b2c88d7c72713b703290ae5ae1d
이원준 <wonjun@MacBook-Pro-2.local>
2023-05-07 13:34:46 +0900

    라우트수정

--- src/Router.js
+++ src/Router.js
@@ -10,12 +10,11 @@ import ProductList from "./pages/ProductList/ProductList";
 import Order from "./pages/Order/Order";
 import Invoice from "./pages/Invoice/Invoice";
 import User from "./pages/User/User";
-import Navbar from "./pages/Invoice/Navbar";
 
 function Router() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/user" element={<User />} />
