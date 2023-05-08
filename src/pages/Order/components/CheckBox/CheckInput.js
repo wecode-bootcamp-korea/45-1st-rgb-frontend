d98c24ecd04813f43e1fda0fd0048c7837a335cc
yoohmoon <ansekfzhd5455@naver.com>
2023-05-08 14:33:14 +0900

    Modify: CheckInput.js 약관동의 체크박스 상수데이터+map

--- src/pages/Order/components/CheckBox/CheckInput.js
+++ src/pages/Order/components/CheckBox/CheckInput.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CheckInput = () => {
+  const [checkInputs, setCheckInputs] = useState([]);
+
+  const checkAllInputs = () => {};
   return (
     <div className="checkInput">
-      <label htmlFor="agreeAll">
-        <input type="checkbox" id="agreeAll" />
-        전체동의
-      </label>
-      <div>
-        <input type="radio" name="privacy" />
-        <div>전체 동의</div>
+      <div className="agreeAllWrap">
+        <label htmlFor="agreeAll">
+          <input type="checkbox" id="agreeAll" checked={false} />
+          전체동의
+        </label>
       </div>
+      {AGREEMENT_TERMS.map(term => {
+        return (
+          <div key={term.id}>
+            <label htmlFor={term.htmlFor}>
+              <input type="checkbox" id={term.htmlFor} />
+              {term.title}
+            </label>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
 export default CheckInput;
+
+const AGREEMENT_TERMS = [
+  {
+    id: 0,
+    title: "본인은 만 14세 이상입니다 (필수)",
+    isChecked: false,
+    htmlFor: "age",
+  },
+  {
+    id: 1,
+    title: "개인정보 수집 및 이용조건에 동의합니다 (필수)",
+    isChecked: false,
+  },
+  {
+    id: 2,
+    title: "주문내역을 확인했으며, 이에 동의합니다 (필수)",
+    isChecked: false,
+  },
+];
