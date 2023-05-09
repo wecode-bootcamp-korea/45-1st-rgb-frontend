5fe81f65b5c033f6e1d81092d628176650a6441d
yoohmoon <ansekfzhd5455@naver.com>
2023-05-10 00:45:25 +0900

    Modify : CheckoutProduct 미구현 UI 제거

--- src/pages/Order/components/OrderList/components/CheckoutProduct.js
+++ src/pages/Order/components/OrderList/components/CheckoutProduct.js
@@ -14,14 +14,8 @@ function CheckoutProduct({ cartItem }) {
       <div className="checkoutProductRight">
         <div className="topRow">
           <h5>{title}</h5>
-          <div className="counter">
-            <button>-</button>
-            <span>{count}</span>
-            <button>+</button>
-          </div>
         </div>
         <div className="bottomRow">
-          <button>삭제</button>
           <span>₩{productPrice}</span>
         </div>
       </div>
