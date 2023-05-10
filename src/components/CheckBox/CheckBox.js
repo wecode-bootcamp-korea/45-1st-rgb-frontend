918b4396a330cddfd248944b7e4297ddd990b364
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-05-10 11:23:27 +0900

    checkbox file 복구

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -3,6 +3,33 @@ import CheckBoxItem from "./CheckBoxItem";
 import "./CheckBox.scss";
 
 const CheckBox = () => {
+  // 체크된 아이템을 담을 배열
+  const [checkItems, setCheckItems] = useState([]);
+
+  // 체크박스 전체 선택
+  const handleAllCheck = checked => {
+    if (checked) {
+      // 전체 선택 클릭 시 데이터의 모든 아이템(id)를 담은 배열로 checkItems 상태 업데이트
+      const idArray = [];
+      CHECKBOX_INFO.forEach(el => idArray.push(el.id));
+      setCheckItems(idArray);
+    } else {
+      // 전체 선택 해제 시 checkItems 를 빈 배열로 상태 업데이트
+      setCheckItems([]);
+    }
+  };
+
+  // 체크박스 단일 선택
+  const handleSingleCheck = (checked, id) => {
+    if (checked) {
+      // 단일 선택 시 체크된 아이템을 배열에 추가
+      setCheckItems(prev => [...prev, id]);
+      setInputValues({ ...inputValues, [name]: value });
+    } else {
+      // 단일 선택 해제 시 체크된 아이템을 제외한 배열 (필터)
+      setCheckItems(checkItems.filter(el => el !== id));
+    }
+  };
   console.log("checkItems", checkItems);
   return (
     <>
