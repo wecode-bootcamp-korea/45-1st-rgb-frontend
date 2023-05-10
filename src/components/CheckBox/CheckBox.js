5b89824009501d42715500ccf0e4198af92c6036
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-05-10 11:31:02 +0900

    체크 박스 복구 완료

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -24,7 +24,6 @@ const CheckBox = () => {
     if (checked) {
       // 단일 선택 시 체크된 아이템을 배열에 추가
       setCheckItems(prev => [...prev, id]);
-      setInputValues({ ...inputValues, [name]: value });
     } else {
       // 단일 선택 해제 시 체크된 아이템을 제외한 배열 (필터)
       setCheckItems(checkItems.filter(el => el !== id));
@@ -33,13 +32,17 @@ const CheckBox = () => {
   console.log("checkItems", checkItems);
   return (
     <>
-      <input
-        type="checkbox"
-        name="selectAll"
-        onChange={e => handleAllCheck(e.target.checked)}
-        // 데이터 개수와 체크된 아이템의 개수가 다를 경우 선택 해제 (하나라도 해제 시 선택 해제)
-        checked={checkItems.length === CHECKBOX_INFO.length ? true : false}
-      />
+      <div className="selectAllBox">
+        <input
+          type="checkbox"
+          name="selectAll"
+          id="selectAll"
+          onChange={e => handleAllCheck(e.target.checked)}
+          // 데이터 개수와 체크된 아이템의 개수가 다를 경우 선택 해제 (하나라도 해제 시 선택 해제)
+          checked={checkItems.length === CHECKBOX_INFO.length ? true : false}
+        />
+        <label for="selectAll">전체 동의</label>
+      </div>
       <div>
         {CHECKBOX_INFO.map((data, key) => (
           <CheckBoxItem
@@ -48,7 +51,6 @@ const CheckBox = () => {
             checkItems={checkItems}
           />
         ))}
-        ;
       </div>
     </>
   );
@@ -58,21 +60,21 @@ const CHECKBOX_INFO = [
   {
     id: 0,
     name: "privacy",
-    children: "개인 정보 동의",
+    checkBoxName: "개인 정보 동의",
   },
   {
     id: 1,
     name: "subscription",
-    children: "뉴스레터 구독",
+    checkBoxName: "뉴스레터 구독",
   },
   {
     id: 2,
     name: "test1",
-    children: "테스트1",
+    checkBoxName: "테스트1",
   },
   {
     id: 3,
     name: "test2",
-    children: "테스트2",
+    checkBoxName: "테스트2",
   },
 ];
