4312a4cc69b17e604eab56bc1a29a24a15a71afc
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-05-10 14:33:51 +0900

    ADD : InvoiceUserData 레이아웃 초기 작업 진행

--- src/pages/Invoice/Invoice.js
+++ src/pages/Invoice/Invoice.js
@@ -1,9 +1,12 @@
 import React from "react";
+import InvoiceUserData from "./InvoiceUserData";
+import InvoiceOrderData from "./InvoiceOrderData";
 import "./Invoice.scss";
 
 function Invoice() {
   return (
     <div className="invoice">
+      <h2>결제가 완료되었습니다.</h2>
       <InvoiceUserData />
       <InvoiceOrderData />
     </div>
--- src/pages/Invoice/InvoiceUserData.js
+++ src/pages/Invoice/InvoiceUserData.js
@@ -1,7 +1,41 @@
 import React from "react";
 
 function InvoiceUserData() {
-  return <div className="InvoiceUserData"></div>;
+  return (
+    <div className="InvoiceUserData">
+      <div className="InvoiceUserDataBox">
+        <p className="InvoiceUserDataSubTitle">주문자 성함</p>
+        <p className="InvoiceUserName">InvoiceUserName</p>
+        <p className="InvoiceUserEmail">InvoiceUserEmail</p>
+      </div>
+      <div className="InvoiceUserDataBox">
+        <p className="InvoiceUserDataSubTitle">주문번호</p>
+        <p className="InvoiceOrderNumber">InvoiceUserName</p>
+      </div>
+      <div className="InvoiceUserDataBox">
+        <p className="InvoiceUserDataSubTitle">배송 정보</p>
+        <p className="InvoiceAddressee">InvoiceAddressee</p>
+        <p className="InvoiceAddress">InvoiceAddress</p>
+        <p className="InvoiceAddressPostNumber">InvoiceAddressPostNumber</p>
+        <p className="InvoiceShippingCharge">InvoiceShippingCharge</p>
+      </div>
+      <ul className="InvoiceUserDataBox">
+        <li className="InvoiceUserDataSubTitle">결제 내역</li>
+        <li className="InvoiceUserPoint">
+          <p>보유 포인트</p>
+          <span>point</span>
+        </li>
+        <li className="InvoiceChargePoint">
+          <p>차감 포인트</p>
+          <span>point</span>
+        </li>
+        <li className="InvoiceSavePoint">
+          <p>남은 가용 포인트</p>
+          <span>point</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default InvoiceUserData;
