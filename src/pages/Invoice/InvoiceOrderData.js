62ee9818d1bc12790e6689bb8977c1b7aa5895a2
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-05-10 14:42:59 +0900

    ADD : InvoiceOrderData 레이아웃 초기 작업 진행중

--- src/pages/Invoice/InvoiceOrderData.js
+++ src/pages/Invoice/InvoiceOrderData.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 function InvoiceOrderData() {
-  return <div className="InvoiceOrderData"></div>;
+  return (
+    <div className="InvoiceOrderData">
+      <p className="InvoiceOrderDataSubTitle">주문 정보</p>
+      <img className="InvoiceProductImage" />
+      <table>
+        <tr>
+          <td>상품명</td>
+          <td>수량</td>
+          <td>가격</td>
+        </tr>
+        <tr>
+          <td>세금</td>
+          <td>세금</td>
+        </tr>
+        <tr>
+          <td>배송비</td>
+          <td>가격</td>
+        </tr>
+        <tr>
+          <td>합계</td>
+          <td>총가격</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 export default InvoiceOrderData;
