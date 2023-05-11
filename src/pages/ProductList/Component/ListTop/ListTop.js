f3816cae345d67568aa854b6e55fabea70b1a648
Jinny-Jin <katie9796@naver.com>
2023-05-11 21:40:05 +0900

    최종

--- src/pages/ProductList/Component/ListTop/ListTop.js
+++ src/pages/ProductList/Component/ListTop/ListTop.js
@@ -10,8 +10,7 @@ function ListTop() {
       </div>
       <img
         alt="List"
-        src="https://github.com/haechan02/rgb_demo_arts/blob/d753962f9aa85bc1508500c92eba6b434ca489db/assets/arts/11.jpeg?raw=true"
-        // src="https://images.unsplash.com/photo-1589442694956-9eaf242dd1fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=868&q=80"
+        src="https://github.com/haechan02/rgb_demo_arts/blob/d753962f9aa85bc1508500c92eba6b434ca489db/assets/arts/arts%209.jpeg?raw=true"
       />
     </div>
   );
