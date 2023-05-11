eae6848f5d58345bf9c7457d578b8b3f219d6d69
Jinny-Jin <katie9796@naver.com>
2023-05-11 23:29:31 +0900

    최종수정

--- src/pages/ProductList/Component/ListTop/ListTop.js
+++ src/pages/ProductList/Component/ListTop/ListTop.js
@@ -8,10 +8,7 @@ function ListTop() {
         <span className="artistOfMay">Artist of May</span>
         <span className="haechan">Hae</span>
       </div>
-      <img
-        alt="List"
-        src="https://github.com/haechan02/rgb_demo_arts/blob/d753962f9aa85bc1508500c92eba6b434ca489db/assets/arts/arts%209.jpeg?raw=true"
-      />
+      <div className="imageImage" />
     </div>
   );
 }
