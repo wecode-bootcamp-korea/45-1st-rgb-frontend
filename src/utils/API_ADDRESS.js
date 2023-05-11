2f040f31da63acb0132404bf53adf36b9acf38e5
cinematicmoment <cinematicmomentz@gmail.com>
2023-05-11 15:47:15 +0900

    checkout

--- src/utils/API_ADDRESS.js
+++ src/utils/API_ADDRESS.js
@@ -1,4 +1,2 @@
-const API_ADDRESS = "http://10.58.52.150:3000/";
-export const API_ADDRESS_ORDERS = "http://10.58.52.169:9001/";
-
-export default API_ADDRESS;
+const API_ADDRESS = "http://10.58.52.169:9001/";
+const API_ADDRESS_ORDERS = "http://10.58.52.169:9001/";
