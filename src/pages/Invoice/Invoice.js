8e4c3e0e9a54a322c4df7edf48c09d27cdc88490
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-07-10 19:04:24 +0900

    invoice fetchApi 로 변경

--- src/pages/Invoice/Invoice.js
+++ src/pages/Invoice/Invoice.js
@@ -2,29 +2,24 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import InvoiceUserData from "./InvoiceUserData";
 import InvoiceOrderData from "./InvoiceOrderData";
-import { API_ADDRESS_ORDERS } from "../../utils/API_ADDRESS";
 import LoadingPage from "../LoadingPage/LoadingPage";
+import { fetchApi } from "../../utils/fetchApi";
 
 import "./Invoice.scss";
 
 function Invoice() {
-  const [invoiceData, setInvoiceData] = useState();
   const params = useParams();
-  const orderId = params.orderNumber;
   const navigate = useNavigate();
-  const token = localStorage.getItem("TOKEN");
+  const orderId = params.orderNumber;
+  const [invoiceData, setInvoiceData] = useState();
+
+  const getOrderData = async () => {
+    const response = await fetchApi(`orders/${orderId}`);
+    setInvoiceData(response);
+  };
 
   useEffect(() => {
-    fetch(`${API_ADDRESS_ORDERS}orders/${orderId}`, {
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        Authorization: token,
-      },
-    })
-      .then(res => res.json())
-      .then(data => {
-        return setInvoiceData(data);
-      });
+    getOrderData();
   }, []);
 
   if (!invoiceData) return <LoadingPage />;
